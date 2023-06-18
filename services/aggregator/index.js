a92f942bc2a41ebf51fe86ba545a15f7f79cf879
Vlad Shalavinskiy <orativtech28@gmail.com>
2023-06-18 20:35:56 +0300

    feat: add retry/timeout

--- services/aggregator/index.js
+++ services/aggregator/index.js
@@ -5,12 +5,17 @@ const app = express()
 const port = 3000
 
 app.get('/api/aggregate', async (req, res) => {
+   try {
    const users = await axios.get('http://local-users:3001/api/users');
    console.log({ users: users.data })
    const cities = await axios.get('http://local-cities:3001/api/cities');
    console.log({ cities: cities.data })
 
   res.json({users: users.data, cities: cities.data});
+   } catch (err) {
+    console.log(err.data)
+    res.json(err.data)
+   }
 });
 
 app.listen(port, () => {
