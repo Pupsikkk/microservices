c46439b066c46c9663a033039a55591f6a890e27
Vlad Shalavinskiy <orativtech28@gmail.com>
2023-06-21 00:54:32 +0300

    feat: connect aggreagator to kafka

--- services/aggregator/index.js
+++ services/aggregator/index.js
@@ -1,10 +1,28 @@
 const { default: axios } = require('axios');
 const express = require('express')
+const { Kafka } = require('kafkajs');
 
 const app = express()
 const port = 3000
 
+const kafka = new Kafka({
+    brokers: ['kafka:9092'],
+});
+const producer = kafka.producer();
+
 app.get('/api/aggregate', async (req, res) => {
+    await producer.send({
+        topic: 'stats',
+        messages: [{
+            value: JSON.stringify({
+                microservice: 'aggregator'
+            })
+        }],
+    });
+    await producer.send({
+        topic: 'log',
+        messages: [{ value: `AGGREGATOR - [DELETE /api/aggregate] ${new Date()}` }],
+    });
    try {
    const users = await axios.get('http://local-users:3001/api/users');
    console.log({ users: users.data })
@@ -18,6 +36,7 @@ app.get('/api/aggregate', async (req, res) => {
    }
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await producer.connect();
     console.log('Server is running on port 3000');
 });
